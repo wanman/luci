383bb714c2d307051dfadc0ac32e6f0a0548c08c
Hostle <hostle19@gmail.com>
2016-02-12 12:21:16 +0200

    firewall: added multi-user support
    - added the user() function to the main index function, this function takes the index entry name as an argument and compares it against a table provided by the hide_menus() functions, if the entry is not in the table then it is omitted from the index ... a simple way to choose what menus should be available in to the users

--- applications/luci-app-firewall/luasrc/controller/firewall.lua
+++ applications/luci-app-firewall/luasrc/controller/firewall.lua
@@ -1,6 +1,29 @@
 module("luci.controller.firewall", package.seeall)
 
 function index()
+	local fs = require "nixio.fs"
+
+         function user(val)
+	   if not fs.access("/usr/lib/lua/luci/users.lua") then return true end
+
+           local dsp = require "luci.dispatcher"
+           local usw = require "luci.users"
+           local user = dsp.get_user()
+	   if user == "root" then return true end
+	   local name = "network"
+
+	   local menu = {}
+	   menu = usw.hide_menus(user,name) or {}
+	   if menu and #menu <= 1 then
+            return false
+           end
+  	   for i,v in pairs(menu) do
+   	     if v == val then return true end
+  	   end
+  	   return false
+	  end
+
+	if user("Firewall") == true then
 	entry({"admin", "network", "firewall"},
 		alias("admin", "network", "firewall", "zones"),
 		_("Firewall"), 60)
@@ -20,4 +43,5 @@ function index()
 	entry({"admin", "network", "firewall", "custom"},
 		cbi("firewall/custom"),
 		_("Custom Rules"), 40).leaf = true
+	end
 end
